3c7bcb99232dfb60b56072e83ecc7c51ab8365e0
tzyfahri015-cpu <tzyfahri015@gmail.com>
2026-07-26 22:26:48 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const btn=document.getElementById("btn");
+const popup=document.getElementById("popup");
+
+btn.onclick=function(){
+
+popup.style.display="flex";
+
+}
+
+function closePopup(){
+
+popup.style.display="none";
+
+}
